603d29015165f8174cf3dd61c1df4e2fb24abe76
OrlovLexy <78743054+OrlovLexy@users.noreply.github.com>
2021-06-13 16:15:51 +0300

    Create UE4 blank project

---
+++ ue4/FortressTown/Source/FortressTown/FortressTown.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FortressTown.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FortressTown, "FortressTown" );
