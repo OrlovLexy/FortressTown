080e56b7c9429759173ab0065415b87a797244e3
OrlovLexy <demonwizard@yandex.ru>
2022-03-31 17:03:32 +0300

    Adding private include paths

--- ue4/FortressTown/Source/FortressTown/FortressTown.Build.cs
+++ ue4/FortressTown/Source/FortressTown/FortressTown.Build.cs
@@ -10,10 +10,12 @@ public class FortressTown : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		PrivateIncludePaths.AddRange(new string[] { Name });
 
 		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		//PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
