3185680a792b887905607b5d7827906693ff3e9b
OrlovLexy <demonwizard@yandex.ru>
2022-03-31 17:06:24 +0300

    [Building] Create base building classes

---
+++ ue4/FortressTown/Source/FortressTown/Actors/Buildings/CraftingActor.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Buildings/BuildingActor.h"
+#include "CraftingActor.generated.h"
+
+UCLASS(Blueprintable)
+class FORTRESSTOWN_API ACraftingActor : public ABuildingActor
+{
+	GENERATED_BODY()
+	
+};
---
+++ ue4/FortressTown/Source/FortressTown/Actors/Buildings/ProductionActor.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Buildings/BuildingActor.h"
+#include "ProductionActor.generated.h"
+
+UCLASS(Blueprintable)
+class FORTRESSTOWN_API AProductionActor : public ABuildingActor
+{
+	GENERATED_BODY()
+	
+};
